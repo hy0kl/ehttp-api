3898f662219d3f9f4225d8eee5270a754e66fcf8
Jerry Yang <hy0kle@gmail.com>
2015-12-28 20:22:34 +0800

    完成主结构体定义.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /**
- * @describe:
+ * @describe: 基于 libehtp 的接口项目
  * @author: Jerry Yang(hy0kle@gmail.com)
+ * @notice: 仅支持 *nix 系统
  * */
 #include "server.h"
 
--- src/server.h
+++ src/server.h
@@ -32,4 +32,82 @@
 #include <zdb/zdb.h>
 #include <zlog.h>
 
+/**
+ _ __ ___   __ _  ___ _ __ ___
+| '_ ` _ \ / _` |/ __| '__/ _ \
+| | | | | | (_| | (__| | | (_) |
+|_| |_| |_|\__,_|\___|_|  \___/
+*/
+/** 配置项目缓冲区长度 */
+#define CONF_BUF_LEN    128
+/** 文件路径缓冲区长度 */
+#define PATH_BUF_LEN    1024
+
+#define SMALL_BUF_LEN       8
+#define CURL_CHUNK_BUF_LEN  10240
+
+#define GETUTIME(t) ((t.tv_sec) * 1000000 + (t.tv_usec))
+#define GETSTIME(t) (t.tv_sec)
+#define GETMTIME(t) ((((t.tv_sec) * 1000000 + (t.tv_usec))) / 1000)
+
+/* Port to listen on. */
+#define SERVER_PORT 5678
+
+/** 全局状态码/错误码 */
+typedef enum _g_error_code_e
+{
+    CAN_NOT_OPEN_SERVER_JSON    = -11,
+    NEED_MORE_MEMORY            = -12,
+    JSON_PARSE_FAILURE          = -13,
+    CAN_NOT_OPEN_ZLOG_CONF      = -1,
+    CAN_NOT_GET_ZLOG_CATEGORY   = -2,
+} g_error_code_e;
+
+/** 全局配置*/
+typedef unsigned int u_int;
+// mysql 节点
+typedef struct _mysql_config_t
+{
+    char   host[CONF_BUF_LEN];
+    u_int  port;
+    char   dbname[CONF_BUF_LEN];
+    char   username[CONF_BUF_LEN];
+    char   password[CONF_BUF_LEN];
+} mysql_config_t;
+
+// redis 节点
+typedef struct _redis_config_t
+{
+    char  host[CONF_BUF_LEN];
+    u_int port;
+    u_int timeout;  /** 超时时间,单位毫秒 */
+} redis_config_t;
+
+typedef struct _server_config_t
+{
+    char  env[SMALL_BUF_LEN];
+    char  ip[CONF_BUF_LEN];
+    u_int port;
+    u_int backlog;
+
+    char zlog_conf[CONF_BUF_LEN];
+    char zlog_category[CONF_BUF_LEN];
+
+    mysql_config_t  mysql_master; /** 主节点只有一个 */
+    u_int           mysql_slaves_count;
+    mysql_config_t *mysql_slaves_array; /** 从库是一组 */
+
+    // redis 不考先不考虑主从,失败重试等复杂情况
+    redis_config_t  redis_storage;  /** redis 存储是单点 */
+    u_int           redis_cache_count;
+    redis_config_t *redis_cache_array;  /** 缓存是一组服务 */
+} server_config_t;
+
+/** 全局变量 */
+extern server_config_t   g_conf;
+extern zlog_category_t  *g_zc;
+extern ConnectionPool_T  mysql_master_pool;
+extern ConnectionPool_T *mysql_slaves_pool;
+
+// redis 不需要链接池
 #endif
